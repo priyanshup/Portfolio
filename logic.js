cc1717c1b19f4c22305fce66e900d0ecca506f68
Priyanshu <priyanshu.dmk@gmail.com>
2024-01-18 13:16:43 +0530

    Savepointt

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 window.onload = function () {
-        // Scroll to the top of the page
-        window.scrollTo(0, 0.01);
-      };
+  // Scroll to the top of the page
+  window.scrollTo(0, 0.01);
+};
+
