db0f7afa9ec589d3eeb306b0366e6d6d954fed2e
Priyanshu <priyanshu.dmk@gmail.com>
2024-02-21 23:47:21 +0530

    Fixed fade issue in the Project section

--- logic.js
+++ logic.js
@@ -1,82 +1,61 @@
-window.onload = function () {
-  // Scroll to the top of the page
-  window.scrollTo(0, 0);
-};
+// Cache selectors
+var $navbar = $(".navbar"),
+  $navbarBrand = $(".navbar-brand"),
+  $navbarNavLinks = $(".navbar-nav .nav-link"),
+  $table = $("table"),
+  $expContentContainer = $("#exp-content-container"),
+  $filterGroup = $(".filter-group");
 
-$(document).ready(function () {
-  // Adjust body padding based on the fixed navbar height
-  $("body").css("padding-top", $(".navbar").outerHeight() + "px");
+// Adjust body padding based on the fixed navbar height
+$("body").css("padding-top", $navbar.outerHeight() + "px");
 
-  // Reload page upon clicking on navbar-brand
-  $(".navbar-brand").click(function () {
-    //Fragment the URL after # and update it back to window
-    window.location.href = window.location.href.split("#")[0];
+// Reload page upon clicking on navbar-brand
+$navbarBrand.click(function () {
+  window.location.href = window.location.href.split("#")[0];
+});
+
+// Add click event handler to all nav-links
+$navbarNavLinks.on("click", function () {
+  $navbarNavLinks.removeClass("active");
+  $(this).addClass("active");
+  // Close the offcanvas menu (if it's open)
+  $("#offcanvasNavbar").offcanvas("hide");
+});
+
+// Add click event handler to all td elements in the table using event delegation
+$table.on("click", "td", function () {
+  $table.find("tr td").removeClass("golden").css("color", "");
+  $(this).addClass("golden");
+  // Get the data-content attribute value
+  updateContent($(this).data("content"));
+});
+
+// Display the default content when the page is loaded
+updateContent("product-owner-content");
+
+function updateContent(content) {
+  $expContentContainer.fadeOut(300, function () {
+    $expContentContainer.empty().html(getContentHtml(content)).fadeIn(300);
   });
+}
 
-  // Add click event handler to all nav-links
-  $(".navbar-nav .nav-link").on("click", function () {
-    // Remove 'active' class from all nav-links
-    $(".navbar-nav .nav-link").removeClass("active");
-
-    // Add 'active' class to the clicked nav-link
-    $(this).addClass("active");
-
-    // Close the offcanvas menu (if it's open)
-    $("#offcanvasNavbar").offcanvas("hide");
-  });
-
-  // Logic to change color on click
-  // Add click event handler to all td elements in the table using event delegation
-  $("table").on("click", "td", function () {
-    // Remove highlighting from all td elements
-    $("table tr td").removeClass("golden").css("color", "");
-
-    // Change the background color of the clicked td
-    $(this).addClass("golden");
-
-    // Get the data-content attribute value
-    var content = $(this).data("content");
-
-    // Update the content in exp-right-module based on the selected content
-    updateContent(content);
-  });
-
-  // Display the default content when the page is loaded
-  updateContent("product-owner-content");
-
-  function updateContent(content) {
-    // Clear previous content
-    $("#exp-content-container").fadeOut(300, function () {
-      $(this).empty();
-
-      // Add new content based on the selected content
-      switch (content) {
-        case "product-owner-content":
-          $("#exp-content-container").html(
-            "<div class='container-fluid'><h5>March 2022 - Present</h5> <h6> TechMojo Solutions | Online Gaming, Sportsbook</h6> <p class='body body-table'>In this universe, I act as a detective, decoding user needs and crafting product solutions. Navigating through ideas and innovations, I find joy in competitor analysis, transforming uncertainty into clarity, and sculpting requirements into the backbone of outstanding products. </p><ul class='exp-list-parent'><li class='exp-list'>HTML</li><li class='exp-list'>CSS</li><li class='exp-list'>Bootstrap</li><li class='exp-list'>jQuery</li><li class='exp-list'>Javascript</li><li class='exp-list'>NodeJS</li><li class='exp-list'>ExpressJS</li><li class='exp-list'>Git</li></ul></div>"
-          );
-          break;
-        case "business-analyst-content":
-          $("#exp-content-container").html(
-            " <div class='container-fluid'> <h5>June 2019 - March 2022</h5> <h6>UnitedHealth Group | US Healthcare</h6> <p class='body body-table'>In this role, I orchestrated successful system launches, provided guidance as a mentor, and served as a dedicated SME. I coordinated the creation of mockups and test cases, turning challenges into well-organized solutions.</p><ul class='exp-list-parent'><li class='exp-list'>PL/SQL</li><li class='exp-list'>JAVA</li><li class='exp-list'>Shell Scripting</li><li class='exp-list'>Tortoise SVN</li></ul></div>"
-          );
-          break;
-        case "software-engineer-content":
-          $("#exp-content-container").html(
-            " <div class='container-fluid'> <h5>July 2016 - June 2019</h5> <h6>UnitedHealth Group | US Healthcare</h6> <p class='body body-table'>As a developer, I seamlessly blended innovation and automation, crafting new modules for US Healthcare that enhanced member enrollment, facilitated plan rollovers, and streamlined premium invoice generation.</p><ul class='exp-list-parent'><li class='exp-list'>PL/SQL</li><li class='exp-list'>JAVA</li><li class='exp-list'>Shell Scripting</li><li class='exp-list'>Tortoise SVN</li></ul></div>"
-          );
-          break;
-        default:
-          break;
-      }
-
-      // Fade in the content
-      $(this).fadeIn(300);
-    });
+// Generate HTML content based on selected content
+function getContentHtml(content) {
+  switch (content) {
+    case "product-owner-content":
+      return "<div class='container-fluid'><h5>March 2022 - Present</h5><h6>TechMojo Solutions | Online Gaming, Sportsbook</h6><p class='body body-table'>In this universe, I act as a detective, decoding user needs and crafting product solutions. Navigating through ideas and innovations, I find joy in competitor analysis, transforming uncertainty into clarity, and sculpting requirements into the backbone of outstanding products.</p><ul class='exp-list-parent'><li class='exp-list'>HTML</li><li class='exp-list'>CSS</li><li class='exp-list'>Bootstrap</li><li class='exp-list'>jQuery</li><li class='exp-list'>Javascript</li><li class='exp-list'>NodeJS</li><li class='exp-list'>ExpressJS</li><li class='exp-list'>Git</li></ul></div>";
+    case "business-analyst-content":
+      return "<div class='container-fluid'><h5>June 2019 - March 2022</h5><h6>UnitedHealth Group | US Healthcare</h6><p class='body body-table'>In this role, I orchestrated successful system launches, provided guidance as a mentor, and served as a dedicated SME. I coordinated the creation of mockups and test cases, turning challenges into well-organized solutions.</p><ul class='exp-list-parent'><li class='exp-list'>PL/SQL</li><li class='exp-list'>JAVA</li><li class='exp-list'>Shell Scripting</li><li class='exp-list'>Tortoise SVN</li></ul></div>";
+    case "software-engineer-content":
+      return "<div class='container-fluid'><h5>July 2016 - June 2019</h5><h6>UnitedHealth Group | US Healthcare</h6><p class='body body-table'>As a developer, I seamlessly blended innovation and automation, crafting new modules for US Healthcare that enhanced member enrollment, facilitated plan rollovers, and streamlined premium invoice generation.</p><ul class='exp-list-parent'><li class='exp-list'>PL/SQL</li><li class='exp-list'>JAVA</li><li class='exp-list'>Shell Scripting</li><li class='exp-list'>Tortoise SVN</li></ul></div>";
+    default:
+      return "";
   }
+}
 
-  // Hover effect over table td without the "golden" class
-  $("table tr td").on({
+// Add mouseover and mouseout event handlers to table td elements
+$table.on(
+  {
     mouseover: function () {
       if (!$(this).hasClass("golden")) {
         $(this).css("color", "#94a3b8");
@@ -84,53 +63,51 @@ $(document).ready(function () {
     },
     mouseout: function () {
       if (!$(this).hasClass("golden")) {
-        $(this).css("color", ""); // Set to an empty string to remove inline style
+        $(this).css("color", "");
       }
     },
-  });
+  },
+  "tr td"
+);
 
-  // Logic to highlight work tabs
-  // Add click event handler to all filter-tabs elements using event delegation
-  $(".filter-group").on("click", ".filter-tab", function () {
-    $(".filter-group .filter-tab").removeClass("active-tab");
-    $(this).addClass("active-tab");
-    var selectedTab = $(this).attr("class").split(" ")[1];
+// Add click event handler to filter-tabs elements using event delegation
+$filterGroup.on("click", ".filter-tab", function () {
+  // Check if the clicked tab is already active
+  if ($(this).hasClass("active-tab")) {
+    return; // Return early without executing further actions
+  }
 
-    // Show all items initially
-    $(".filter-projects .project-content").show();
+  var selectedTab = $(this).attr("class").split(" ")[1];
+  $(".filter-group .filter-tab").removeClass("active-tab");
+  $(this).addClass("active-tab");
 
-    switch (selectedTab) {
-      case "uhg-tab":
-        $(".filter-projects .project-content:not(.uhg-project)").fadeOut();
-        break;
-      case "techmojo-tab":
-        $(".filter-projects .project-content:not(.techmojo-project)").fadeOut();
-        break;
-      case "personal-tab":
-        $(".filter-projects .project-content:not(.personal-project)").fadeOut();
-        break;
-      default:
-        // Show all project contents with fade-in effect
-        $(".filter-projects .project-content").fadeIn();
-        break;
-    }
-  });
+  // Hide all project contents
+  $(".filter-projects .project-content").hide();
 
-  // Check if the user has scrolled down
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-      // If scrolled down, show the "Move to Top" button
-      $("#moveToTopBtn").addClass("show");
-    } else {
-      // If at the top, hide the "Move to Top" button
-      $("#moveToTopBtn").removeClass("show");
-    }
-  });
+  switch (selectedTab) {
+    case "uhg-tab":
+      $(".uhg-project").fadeIn();
+      break;
+    case "techmojo-tab":
+      $(".techmojo-project").fadeIn();
+      break;
+    case "personal-tab":
+      $(".personal-project").fadeIn();
+      break;
+    default:
+      // Show all project contents if no specific tab is selected
+      $(".filter-projects .project-content").fadeIn();
+      break;
+  }
+});
 
-  // Scroll to the top when the button is clicked
-  $("#moveToTopBtn").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, "fast");
-    return false;
-  });
-  
+// Show/hide "Move to Top" button based on scroll position
+$(window).scroll(function () {
+  $("#moveToTopBtn").toggleClass("show", $(this).scrollTop() > 50);
+});
+
+// Scroll to top when "Move to Top" button is clicked
+$("#moveToTopBtn").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, "fast");
+  return false;
 });
